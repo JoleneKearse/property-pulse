bf99e88355b7a92ef39bcb08e9b2cef20018e6fd
JoleneKearse <pharaohnutz@gmail.com>
2025-02-02 16:00:52 -0500

    starting done

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,14 @@
+import Hero from "../components/Hero";
+import HomeProperties from "../components/HomeProperties";
+import InfoBoxes from "../components/InfoBoxes";
+
 const HomePage = () => {
   return (
-    <div className="text-2xl">Home Page</div>
+    <>
+      <Hero />
+      <InfoBoxes />
+      <HomeProperties />
+    </>
   );
 }
 
